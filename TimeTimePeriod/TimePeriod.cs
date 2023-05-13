add0d61af163d22fae5b54a67d821c713cb6e580
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2023-05-13 15:33:34 +0200

    commit

--- TimeTimePeriod/TimePeriod.cs
+++ TimeTimePeriod/TimePeriod.cs
@@ -1,17 +1,53 @@
 namespace TimeTimePeriod;
 
-public struct TimePeriod
+public struct TimePeriod:IEquatable<TimePeriod>,IComparable<TimePeriod>
 {
     private  long  TotalSeconds;
 
-    public TimePeriod(int hours , int minutes, int seconds)
+    public TimePeriod(long hours , long minutes , long seconds)
     {
         if (hours < 0 || minutes < 0 || minutes > 60 || seconds < 0 || seconds > 60) throw new ArgumentException("invalid data");
        
         TotalSeconds = hours*3600+minutes*60+seconds;
     }
 
-    public TimePeriod(int hours, int minutes) : this(hours, minutes, 0) { }
-    public TimePeriod(int hours) : this(hours, 0, 0) { }
+    public TimePeriod(long hours, long minutes) : this(hours, minutes, 0) { }
+    public TimePeriod(long hours) : this(hours, 0, 0) { }
 
+    public  TimePeriod(string TimePeriodString)
+    {
+        var parts = TimePeriodString.Split(":");
+        if (parts.Length != 3) throw new ArgumentException("invalid data");
+        if (!long.TryParse( parts[0],out long hours ) || !long.TryParse(parts[1],out long minutes) || !long.TryParse(parts[3], out long seconds))
+        {
+            throw new ArgumentException("invalid data");
+
+        }
+        TotalSeconds = hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public override string ToString()
+    {
+        return $"{TotalSeconds / 3600}:{(TotalSeconds / 60) % 60}:{TotalSeconds % 60} ";
+    }
+
+    public bool Equals(TimePeriod other)
+    {
+        return TotalSeconds == other.TotalSeconds;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is TimePeriod other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return TotalSeconds.GetHashCode();
+    }
+
+    public int CompareTo(TimePeriod other)
+    {
+        return TotalSeconds.CompareTo(other.TotalSeconds);
+    }
 }
